d5b5edf2c64ce5e2a151ef98a64dcc61dc0cd73b
freecates <ramongil@ramongil.cat>
2017-08-23 12:09:16 +0200

    revisat el serviceWorker.register a /

--- pages/service-worker.js
+++ pages/service-worker.js
@@ -25,7 +25,7 @@ const swProdRegistration =
 	window.addEventListener('load', function() {
 		if ('serviceWorker' in navigator &&
 				(window.location.protocol === 'https:' || isLocalhost)) {
-			navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+			navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
 			}).catch(function(e) {
 				console.error('Error during service worker registration:', e)
