c610dc9432f8a8ed5ca40e71844dc852650e7210
freecates <ramongil@ramongil.cat>
2017-11-07 09:48:00 +0100

    refinat el IsMember estils i comportament

--- components/IsMember.js
+++ components/IsMember.js
@@ -1,5 +1,6 @@
 import btoa from 'btoa'
 import fetch from 'isomorphic-unfetch'
+import Link from 'next/link'
 
 class IsMember extends React.Component {
     constructor(props) {
@@ -46,13 +47,37 @@ class IsMember extends React.Component {
         const isRegistered = await res.json()
         this.setState({
             isRegistered: isRegistered.Response
-        })
-        
+        })       
       event.preventDefault();
     }
   
     render() {
-        if (this.state.isRegistered != true) {
+        console.log(`Hola: ${this.state.isRegistered}`)
+        if (this.state.isRegistered == true) { 
+            return (
+                <section>
+                    <div className='callout large alert fade-in'>
+                        <p>¡ATENCIÓN!: <span>{this.state.HowTo}</span></p>
+                    </div>
+                        <style jsx>{`
+                            .fade-in {
+                            animation-name: fadeIn;
+                            animation-duration: 1.3s;
+                            animation-timing-function: cubic-bezier(0, 0, 0.4, 1);
+                            animation-fill-mode: forwards;
+                            }
+                            @keyframes fadeIn {
+                            from {
+                                opacity: 0;
+                            }
+                            to {
+                                opacity: 1;
+                            }
+                            }
+                        `}</style>
+                </section>
+            );
+        } else 
             return (
                 <section>
                     <div className='wrapper'>
@@ -60,13 +85,15 @@ class IsMember extends React.Component {
                         <form>
                         <label>
                             Correo electrónico:
-                            <input type="email" name='isEmail' value={this.state.isEmail} onChange={this.handleChange} />
+                            <input type="email" name='isEmail' value={this.state.isEmail} onChange={this.handleChange} required />
                         </label>
                         <label>
                             Contrasenya:
-                            <input type="password" name='isPassword' value={this.state.isPassword} onChange={this.handleChange} />
+                            <input type="password" name='isPassword' value={this.state.isPassword} onChange={this.handleChange}required />
                         </label>
                         <div className='wrapper-input'><input type="button" className='button' onClick={this.handleSubmit} value="Enviar" /></div>
+                        <p className='yellow margin-inverse'>¿Quieres participar de estos beneficios? Ahora te puedes hacer socio. ¡Fácil y rápido!</p>
+                        <div className='wrapper-input'><Link href='/hazte-socio' prefetch><a className='button button-pink' >Sí, quiero hacerme socio</a></Link></div>
                         </form>
                     </div>
                     <style jsx>{`
@@ -79,6 +106,7 @@ class IsMember extends React.Component {
                         form {
                         padding: 2em;
                         background: #333333;
+                        border-radius:6.5%;
                         }
                         label {
                         color:#ffffff;
@@ -96,8 +124,25 @@ class IsMember extends React.Component {
                         input[type=button]:hover {
                         background:#007e2a;
                         }
-                        h1, label {
+                        .button-pink {
+                        background:#cc3366;
+                        margin:0 auto;
+                        display:block;
+                        width:50%;
+                        color:#ffffff!important;
+                        }
+                        .button-pink:hover {
+                        background:#a62953;
+                        }
+                        h1, label, p {
                             text-align:center;
+                        }
+                        .yellow {
+                            color:#f3f303;
+                        }
+                        .margin-inverse {
+                            margin-top:1em;
+                            margin-bottom:0;
                         }
                         .file-label {
                             background:#cc0033;
@@ -106,15 +151,23 @@ class IsMember extends React.Component {
                             padding:1em;
                             white-space:normal;
                         }
+                        .fade-in {
+                          animation-name: fadeIn;
+                          animation-duration: 1.3s;
+                          animation-timing-function: cubic-bezier(0, 0, 0.4, 1);
+                          animation-fill-mode: forwards;
+                        }
+                        @keyframes fadeIn {
+                          from {
+                            opacity: 0;
+                          }
+                          to {
+                            opacity: 1;
+                          }
+                        }
                     `}</style>
                 </section>
-            );
-        } else 
-        return (
-            <div className='callout large alert'>
-                <p>¡ATENCIÓN!: <span>{this.state.HowTo}</span></p>
-            </div>
-        ); 
+            ); 
     }
   }
   export default IsMember
