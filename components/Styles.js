9f0219cbe2497c2726f1c4fe66058a08d1ea8d0a
freecates <ramongil@ramongil.cat>
2017-11-17 16:57:27 +0100

    passat el awesome als styles

--- components/Styles.js
+++ components/Styles.js
@@ -7,6 +7,7 @@ const Styles = () => (
         <link rel="stylesheet" href="/static/foundation.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet" />
         <link rel="stylesheet" href="/static/global.css" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
     </section>
 )
 
