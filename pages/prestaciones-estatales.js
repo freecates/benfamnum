8bd3cb61658ea3485dd23de780f212e434590e10
freecates <ramongil@ramongil.cat>
2018-01-23 14:54:49 +0100

    el·liminada la prestació estatal de carnet famnum

--- pages/prestaciones-estatales.js
+++ pages/prestaciones-estatales.js
@@ -60,20 +60,6 @@ const PrestacionesEstatales = (props) => (
       </IntlProvider>
       <section>
         <Observer threshold={1} triggerOnce={true} render={() => (<figure className='fade-in'><img src='/static/girl-516341_1920.jpg' width='100%' height='324' alt='Imagen prestaciones estatales Familias Numerosas' title='Imagen prestaciones estatales Familias Numerosas'/></figure>)} />
-
-            <h2>Título de Familia Numerosa</h2>
-            
-            <p className='text-left'>El Título de Familia Numerosa o carné de familia numerosa es el documento oficial que te acredita como familia numerosa para poder acceder a los beneficios de carácter público que te corresponden por Ley. Este documento lo expiden las Comunidades Autónomas y puedes solicitarlo en el momento en que te conviertas en familia numerosa, es decir, al nacer tu tercer hijo o si te encuentras en alguno de los supuestos que contempla la <Link href='http://www.familiasnumerosas.org/fileadmin/user_upload/PDF/leyFFNN2003.pdf'><a target='_blank'>Ley de Protección a las Familias Numerosas</a></Link> para ser considerado familia numerosa: dos hijos uno de ellos con discapacidad, o discapacidad de uno de los padres; viudos con dos hijos, etc.</p>
-
-            <p className='text-left'>Existen dos categorías de familia numerosa:</p>
-
-            <ul className='text-left'>
-                <li>Familia Numerosa General: familias de hasta 4 hijos (*)</li>
-                <li>Familia Numerosa Especial: familias con 5 o más hijos.</li>
-            </ul>
-            <p className='text-left'>Según pertenezcas a una u otra, tendrás más o menos beneficios.</p>
-
-            <p className='text-left'>(*) Las familias de 4 hijos cuyos ingresos anuales, divididos por el número de miembros de la unidad familiar, no superen en cómputo anual el 75 por ciento del IPREM (Indicador Público de Efectos Múltiples), incluidas las pagas extraordinarias, tendrán la consideración de familias numerosas de Categoría Especial.</p>
       </section>
     </section>
         <style jsx>{`
