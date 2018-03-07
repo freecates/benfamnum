20d06fb780d2535be685d26af58211ada4374001
freecates <ramongil@ramongil.cat>
2018-03-07 09:57:03 +0100

    canvis de links

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,8 @@ export default () => (
     </Head>
         <section className='call-to-action'>
                 <h1><img alt='Icono conseguir beneficios familias numerosas' src='/static/icona-conseguir-beneficios-familias-numerosas.png'/><br/>¿Eres socio? ¿Quieres conseguir estos descuentos?</h1>
-                <p>Ser familia numerosa te ofrece la posibilidad de muchas ventajas al asociarte. Participa y disfruta de todas ellas de una manera fácil y rápida.</p>
                 <p><img alt='Carnet de socio familias numerosas' src='/static/carnet-familias-numerosas.jpg'/></p>
-                <p><Link href="http://www.familias-numerosas.org/"><a className='hollow button' target='_blank' title='Enlace externo' rel='noopener'>Quiero participar de estas ventajas</a></Link></p>
+                <p><Link href="http://www.familiasnumerosas.org/hazte-socio/"><a className='hollow button' target='_blank' title='Enlace externo' rel='noopener'>Quiero participar de estas ventajas</a></Link></p>
                 <style jsx>{`
                     .call-to-action {
                         text-align:center;
