3ecaccb35f21a4e5f9a00696f3690b4d847496ca
Ramon Gil <ramon.gil@deporvillage.com>
2021-12-06 14:55:23 +0100

    with-latest-next-12 , upgraded to latest next and some dependencies

--- sitemap-generator.js
+++
@@ -1,8 +0,0 @@
-const sitemap = require('nextjs-sitemap-generator');
-
-sitemap({
-  baseUrl: 'https://beneficiosfamiliasnumerosas.org',
-  ignoredPaths: ['admin'],
-  pagesDirectory: 'pages',
-  targetDirectory: 'static/'
-});
